fa69bf6fc12bddbf15a8162e852ecf8655dbbe9c
gbasit4u <gbasit4u@gmail.com>
2019-03-04 19:34:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req,res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-type','text/html');
+	res.end('Hello World!');
+});
+
+server.listen(port,hostname, ()=> {
+	console.log('listening on port : '+port);
+});
